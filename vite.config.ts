13848e6a3ec4434a14b3454ef0016e1df021ef0c
François Dabiel <dabielf@gmail.com>
2025-02-08 12:10:07 -0600

    edit vite config for externals

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		// Mark the module as external for SSR
+		external: ["@node-rs/argon2-wasm32-wasi"],
+	},
+	build: {
+		rollupOptions: {
+			// Also mark it as external during bundling
+			external: ["@node-rs/argon2-wasm32-wasi"],
+		},
+	},
 });
